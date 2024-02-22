080531a1d0a8f9db2a400c7aef9b605ed58453d2
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-02-22 20:16:30 -0300

    :sparkles: creating base search

---
+++ src/components/Search.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input type="text" placeholder="Busque em suas notas..." 
+      class="w-full text-3xl text-[#e2e8f0] border-none bg-transparent font-bold"
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
