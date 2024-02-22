bc92606cd76285a4397b75235b33edd6123291d8
Aldo Wanderley Costa Junior <aldowanderley11@gmail.com>
2024-02-22 20:16:15 -0300

    :sparkles: creating base notes

---
+++ src/components/Notes.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="max-w-[348px] h-[250px] bg-gradient-to-b from-[#1e293b] p-5 rounded-md">
+    <span class="text-sm text-[#e2e8f0]">há 2 dias</span>
+    <p class="text-sm leading-6 text-[#94a3b8] mt-3">
+      O Drizzle possui um plugin do ESLint para evitar que realizemos updates ou deletes sem where...
+      
+      Para configurar o plugin, é preciso instalar como abaixo:
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
